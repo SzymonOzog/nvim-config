8ea7a7c619b166148a1240397c11896502a1ea9c
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-02-07 18:40:46 +0100

    configured harpoon

--- lua/harpoonBindings.lua
+++ lua/harpoonBindings.lua
@@ -27,11 +27,16 @@ vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
 vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end, {desc = '[m]ark for harpoon'})
 vim.keymap.set("n", "<leader>H", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = '[H]arpoon quick menu'})
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end,{desc = 'harpoon tab [1]'})
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end,{desc = 'harpoon tab [2]'})
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end,{desc = 'harpoon tab [3]'})
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end,{desc = 'harpoon tab [4]'})
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end,{desc = 'harpoon tab [5]'})
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end,{desc = 'harpoon tab [6]'})
+vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end,{desc = 'harpoon tab [7]'})
+vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end,{desc = 'harpoon tab [8]'})
+vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end,{desc = 'harpoon tab [9]'})
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-m>", function() harpoon:list():next() end)
