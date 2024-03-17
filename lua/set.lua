e76ab57bbc26a472c91b57e24f8ee6515befb328
szymonozog <szymon.ozog@gmail.com>
2024-03-17 15:46:56 +0100

    add dap mappings + docs

--- lua/set.lua
+++ lua/set.lua
@@ -13,7 +13,7 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
-vim.opt.undodir = os.getenv("APPDATA") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = true
