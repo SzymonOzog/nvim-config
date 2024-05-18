35d99a9532d25087ae1975784f5c91a913b0ef7f
SzymonOzog <szymon.ozog@gmail.com>
2024-05-18 10:25:07 +0200

    lsp find references

--- lua/remap.lua
+++ lua/remap.lua
@@ -17,7 +17,7 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').lsp_references, { desc = '[S]earch [R]eferences' })
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndootree' })
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tuff' })
 vim.keymap.set('n', '<esc>', vim.cmd.noh)
