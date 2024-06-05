4bc816ee605d2fde00dc247efd7629a855b62743
Szymon <szymon.ozog@gmail.com>
2024-06-05 11:11:33 +0200

    dap config for haskell

--- lua/dap_config.lua
+++ lua/dap_config.lua
@@ -11,6 +11,30 @@ dap.adapters.lldb = {
   name = 'lldb'
 }
 
+dap.adapters.haskell = {
+  type = 'executable';
+  command = 'haskell-debug-adapter';
+  args = {'--hackage-version=0.0.33.0'};
+}
+
+dap.configurations.haskell = {
+  {
+    type = 'haskell',
+    request = 'launch',
+    name = 'Debug',
+    workspace = '${workspaceFolder}',
+    startup = "${file}",
+    stopOnEntry = true,
+    logFile = vim.fn.stdpath('data') .. '/haskell-dap.log',
+    logLevel = 'WARNING',
+    ghciEnv = vim.empty_dict(),
+    ghciPrompt = "λ: ",
+    -- Adjust the prompt to the prompt you see when you invoke the stack ghci command below 
+    ghciInitialPrompt = "ghci> ",
+    ghciCmd= "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show",
+  },
+}
+
 dap.configurations.zig = {
   {
     name = 'Launch',
