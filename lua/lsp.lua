bae8f0f697252154149043a8432ddaefdaf73a73
SzymonOzog <szymon.ozog@aleph-alpha.com>
2024-10-10 16:25:38 +0000

    check for conda enviroments

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,19 @@
+local function isempty(s)
+  return s == nil or s == ""
+end
+local function use_if_defined(val, fallback)
+  return val ~= nil and val or fallback
+end
+
+local conda_prefix = os.getenv("CONDA_PREFIX")
+if not isempty(conda_prefix) then
+  vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
+  vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+else
+  vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
+  vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
+end
+
 local util = require 'lspconfig.util'
 local function switch_source_header()
   bufnr = 0
